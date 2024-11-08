8f618e6a67c9c03a60b91f88a170753557703d3d
doxcom <aldoperez1319@gmail.com>
2024-11-08 00:34:23 -0600

    refactoring mock test, moving service mock line and methods outside test method

--- src/test/java/com/example/unit_testing/business/SomeBusinessMockTest.java
+++ src/test/java/com/example/unit_testing/business/SomeBusinessMockTest.java
@@ -1,6 +1,7 @@
 package com.example.unit_testing.business;
 
 import com.example.unit_testing.data.SomeDataService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,14 +12,19 @@ import static org.mockito.Mockito.when;
 
 class SomeBusinessMockTest {
 
+    SomeBusinessImpl business = new SomeBusinessImpl();
+    SomeDataService dataServiceMock = mock(SomeDataService.class);//we need to create a mock from a service
+
+    @BeforeEach
+    public void before(){
+        business.setSomeDataService(dataServiceMock);
+    }
+
     @Test
     public void calculateSumUsingDataService_basic(){
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        //we need to create a mock from a service
-        SomeDataService dataServiceMock = mock(SomeDataService.class);
+
         //we test directly by using mockito methods
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
-        business.setSomeDataService(dataServiceMock);//this stub is calling from line 8
         int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 6;
         assertEquals(expectedResult, actualResult);
@@ -29,11 +35,7 @@ class SomeBusinessMockTest {
 
     @Test
     public void calculateSumUsingDataService_isEmpty(){
-        //we declare the class component to test
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        SomeDataService dataServiceMockEmpty = mock(SomeDataService.class);
-        when(dataServiceMockEmpty.retrieveAllData()).thenReturn(new int[] {});
-        business.setSomeDataService(dataServiceMockEmpty);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
         int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 0;
         assertEquals(expectedResult, actualResult);
@@ -41,11 +43,7 @@ class SomeBusinessMockTest {
 
     @Test
     public void calculateSumUsingDataService_oneValue(){
-        //we declare the class component to test
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        SomeDataService dataServiceMockOne = mock(SomeDataService.class);
-        when(dataServiceMockOne.retrieveAllData()).thenReturn(new int[] {1});
-        business.setSomeDataService(dataServiceMockOne);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1});
         int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 1;
         assertEquals(expectedResult, actualResult);
