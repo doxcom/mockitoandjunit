ab75d33ec41e68282ad56f96ede9c7c961feeb46
doxcom <aldoperez1319@gmail.com>
2024-11-11 17:45:36 -0600

    testing with parameters

---
+++ src/test/java/com/example/unit_testing/business/ListMockTest.java
@@ -0,0 +1,38 @@
+package com.example.unit_testing.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_basic(){
+        List mock = mock(List.class);
+        when(mock.size()).thenReturn(5);
+        assertEquals(5,mock.size());
+    }
+
+
+    @Test
+    public void returnDifferentValues(){
+        List mock = mock(List.class);
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5,mock.size());
+        assertEquals(10,mock.size());
+
+    }
+
+
+    @Test
+    public void returnWithParameters(){
+        when(mock.get(0)).thenReturn("in28minutes");
+        assertEquals("in28minutes",mock.get(0));
+    }
+}
