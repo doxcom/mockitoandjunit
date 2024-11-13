b96c55b2ba37d0c4ac179467d8dbff63600298df
doxcom <aldoperez1319@gmail.com>
2024-11-12 23:13:52 -0600

    test with generic parameters, wit anyInt method

--- src/test/java/com/example/unit_testing/business/ListMockTest.java
+++ src/test/java/com/example/unit_testing/business/ListMockTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyInt;
 
 public class ListMockTest {
 
@@ -34,5 +35,14 @@ public class ListMockTest {
     public void returnWithParameters(){
         when(mock.get(0)).thenReturn("in28minutes");
         assertEquals("in28minutes",mock.get(0));
+        assertEquals(null,mock.get(1));
+    }
+
+
+    @Test
+    public void returnWithGenericParameters(){
+        when(mock.get(anyInt())).thenReturn("in28minutes");
+        assertEquals("in28minutes",mock.get(0));
+        assertEquals("in28minutes",mock.get(1));
     }
 }
