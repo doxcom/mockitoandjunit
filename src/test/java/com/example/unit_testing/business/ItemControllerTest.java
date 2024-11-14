93009b1678d8e4cbb15e19762728bda955c456b9
doxcom <aldoperez1319@gmail.com>
2024-11-13 23:15:05 -0600

    testing a json response instead a string

---
+++ src/test/java/com/example/unit_testing/business/ItemControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.unit_testing.business;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = ItemController.class)
+class ItemControllerTest {
+  //only test this specific controller
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void dummyItem_basic() throws Exception {
+        //make a call to helloworld url
+        //and verify the response hello world string
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+
+    }
+}
